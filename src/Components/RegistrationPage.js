379cb06a974b0dd88a31d24b67ff69f8b6e75a00
Rajasekhar Dronamraju <153741821+Rajasekhar318@users.noreply.github.com>
2024-06-12 06:57:35 +0530

    Update RegistrationPage.js

--- src/Components/RegistrationPage.js
+++ src/Components/RegistrationPage.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './RegistrationPage.css'; 
 
+// RegistrationPage component for handling new user registrations
 const RegistrationPage = () => {
+  // Hook to navigate programmatically
   const navigate = useNavigate();
+  // State to store user input for registration
   const [user, setUser] = useState({
     username: '',
     password: '',
     email: ''
   });
 
+  // Function to handle changes in the input fields and update state
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser(prevState => ({
@@ -18,19 +22,26 @@ const RegistrationPage = () => {
     }));
   };
 
+   // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Retrieve existing users from localStorage or initialize to an empty array
     const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+    // Check if the username already exists among existing users
     if (existingUsers.some(existingUser => existingUser.username === user.username)) {
       alert('Username already exists. Please choose a different username.');
     } else {
+      // Add the new user to the array of existing users
       existingUsers.push(user);
+      // Save the updated array of users to localStorage
       localStorage.setItem('users', JSON.stringify(existingUsers));
       alert('Registration successful!');
+      // Navigate to the login page after successful registration
       navigate('/login');
     }
   };
 
+  // Render the registration form
   return (
     <div className="container">
       <form onSubmit={handleSubmit} className="form">
